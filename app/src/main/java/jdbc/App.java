ec7b0d44bf6b22f35819a1018f1882440337cebc
daBoss <serge.loie@yandex.ru>
2023-12-16 09:48:40 +0500

    try with resources

--- app/src/main/java/jdbc/App.java
+++ app/src/main/java/jdbc/App.java
@@ -9,30 +9,29 @@ import java.sql.SQLException;
 public class App {
 
 
-    public static void main(String[] args) throws SQLException{
+    public static void main(String[] args) throws SQLException {
 
-        var conn = DriverManager.getConnection("jdbc:h2:mem:heclet_test");
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:heclet_test")) {
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '1234567890')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
 
-        var sql3 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
-        var resultSet = statement3.executeQuery(sql3);
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '1234567890')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
+
+            var sql3 = "SELECT * FROM users";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
         }
-
-        statement3.close();
-
-        conn.close();
     }
 }
